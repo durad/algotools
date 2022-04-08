b28431d5245405e7ccfa86bb5f5827febe9b84f8
Dusan Radovanovic <dradovanovic@stackoverflow.com>
2022-04-07 23:44:16 -0700

    Added exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+export { matrixCreate } from './matrix-create';
+export { matrixForeach } from './matrix-foreach';
+export { matrixMap } from './matrix-map';
+export { matrixReduce } from './matrix-reduce';
+export { matrixPrint, matrixToStr } from './matrix-print';
+export { readLine } from './read-line';
