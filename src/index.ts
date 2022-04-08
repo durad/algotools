56c1fcf832e3f1844376b92db651d222f163b5c5
Dusan Radovanovic <dradovanovic@stackoverflow.com>
2022-04-08 14:24:20 -0700

    Exporting bin search

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 
+export { readLine } from './read-line';
 export { matrixCreate } from './matrix-create';
 export { matrixForeach } from './matrix-foreach';
 export { matrixMap } from './matrix-map';
 export { matrixReduce } from './matrix-reduce';
 export { matrixPrint, matrixToStr } from './matrix-print';
-export { readLine } from './read-line';
+export { binSearchFirstPass, binSearchLastFail, binSearchFirstFail, binSearchLastPass } from './bin-search';
